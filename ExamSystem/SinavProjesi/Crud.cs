d1d36db93498146167775971ea223e7338d725a7
Talha Gergin <talhagergin6@gmail.com>
2022-05-18 17:30:35 +0300

    Exam System
    Exam system project

---
+++ ExamSystem/SinavProjesi/Crud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SinavProjesi
+{
+   public class Crud
+    {
+        static DataTable dt;
+        public static DataTable List(string sql)
+        {
+            dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, Connect.con);
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
